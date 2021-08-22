d84b6428cb86502f9ebc2162b06c7171a63b5cf5
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-22 12:01:24 +0100

    Create ist.COMPILE.bat

---
+++ ist.COMPILE.bat
@@ -0,0 +1,12 @@
+(compile AsacScript)
+sc fg in on y
+ad s 
+processMouseInput
+Add mouse X Mouse Y Mouse 2d m.jpg
+ac s
+ls -Force
+on jk
+io
+iso = ex .wallpaper IN src sd gh ty i
+asdighoid.k
+in S

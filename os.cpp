2e92528068eac885ae4ff94a171f2551df36c525
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-22 12:09:26 +0100

    Update os.cpp

---
+++ os.cpp
@@ -0,0 +1,32 @@
+{compile} fg CONFIG SapScript
+<DocType>
+sc sc ty us
+ls fd
+on sc blueScreen
+sc insrall OOBE
+background.jpg
+CPP G
+on bav
+reect gf blck on 78 90;
+on StartMenu.jpg Toggle -Force SlideUp OR SlideDown
+start.jpg ty i9
+0 apps on hj browser = io.kl
+calc = calc.kl
+calender = cal.kl
+MetroApps = ALL metroaps.kl
+SystemControl = control.kl
+kli = 0o.io
+  io
+  ls
+Immrersivev -Op -0Add
+dd
+on io ls rq ws gh
+br =kl
+onj
+add [op] {l}
+{"#^RASTU"}
+sc
+rsc
+LITRE EXCEPT ON JH add re,move
+xt rt sa fg
+00999r
